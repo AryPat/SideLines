7c51df91e296afe625e3ace12ce9658d8f0408c3
Arypat <aryan.patel70@live.ca>
2024-01-08 15:05:32 -0500

    fix: bug where order does not have any attributes

--- backend/validators.js
+++ backend/validators.js
@@ -15,24 +15,23 @@ const check_attribute_exists = (attr) => {
 
 const json_validation = (body, res) => {
   valid = true;
+  error = "";
 
   if ("order" in body) {
+    if (!body["order"].length) {
+      err = "You need to include attributes you want to order by.";
+      valid = false;
+    }
     body["order"].forEach((element) => {
       if (element.length !== 2) {
-        res.status(400).json({
-          error: "Order format is invalid! It should have exactly 2 elements.",
-        });
+        err = "Order format is invalid! It should have exactly 2 elements.";
         valid = false;
       } else if (!check_attribute_exists(element[0])) {
-        res.status(400).json({
-          error: `Attribute "${element[0]}" does not exist.`,
-        });
+        err = `Attribute "${element[0]}" does not exist.`;
         valid = false;
       } else if (!["ASC", "DESC"].includes(element[1])) {
-        res.status(400).json({
-          error: `Invalid order direction "${element[1]}". 
-                  Please use 'ASC' or 'DESC'.`,
-        });
+        err = `Invalid order direction "${element[1]}". 
+                  Please use 'ASC' or 'DESC'.`;
         valid = false;
       }
     });
@@ -41,13 +40,16 @@ const json_validation = (body, res) => {
   if ("where" in body) {
     Object.keys(body["where"]).forEach((attr) => {
       if (!check_attribute_exists(attr)) {
-        res.status(400).json({ error: `Attribute "${attr}" does not exist.` });
+        err = `Attribute "${attr}" does not exist.`;
         valid = false;
       }
     });
   }
-
-  return valid;
+  if (!valid) {
+    res.status(400).json({ error: err });
+    return false;
+  }
+  return true;
 };
 
 module.exports = { check_json_req, json_validation };
