40b2a2e6a2a024bccc295e784666f41792fb01a8
Arypat <aryan.patel70@live.ca>
2024-01-09 15:45:05 -0500

    feat: add minimal scroll bar

--- frontend/src/Groups.jsx
+++ frontend/src/Groups.jsx
@@ -12,6 +12,7 @@ import {
   HStack,
   Text,
   Badge,
+  Box,
 } from "@chakra-ui/react";
 
 const members = {
@@ -44,66 +45,70 @@ const members = {
 
 function Groups() {
   return (
-    <Flex width="100%" direction="column">
+    <Flex width="100%" height="100%" direction="column">
       <Heading
         fontSize="20px"
         as="b"
         color="Black"
         width="100%"
         opacity="100"
-        paddingBottom="1rem"
+        paddingBottom="0.5rem"
         textAlign="left"
       >
         The Groups
       </Heading>
-      <Accordion allowToggle width="100%">
-        {Object.entries(members).map((key) => (
-          <AccordionItem key={key[0]}>
-            <AccordionButton>
-              <Heading fontSize="md" as="i" flex="1" textAlign="left">
-                {key[0]}
-              </Heading>
-              <AvatarGroup size="sm">
-                {Object.entries(members[key[0]]).map(([name, src]) => (
-                  <Avatar key={name} name={name} src={src} />
-                ))}
-              </AvatarGroup>
-              <AccordionIcon />
-            </AccordionButton>
+      <Box height="100%" overflow="auto">
+        <Accordion allowToggle width="100%">
+          {Object.entries(members).map((key) => (
+            <AccordionItem key={key[0]}>
+              <AccordionButton>
+                <Heading fontSize="md" as="i" flex="1" textAlign="left">
+                  {key[0]}
+                </Heading>
+                <AvatarGroup size="sm">
+                  {Object.entries(members[key[0]]).map(([name, src]) => (
+                    <Avatar key={name} name={name} src={src} />
+                  ))}
+                </AvatarGroup>
+                <AccordionIcon />
+              </AccordionButton>
 
-            <AccordionPanel>
-              <VStack>
-                {Object.entries(members[key[0]]).map(([name, src], index) => (
-                  <Flex
-                    key={key[0] + index}
-                    width="100%"
-                    backgroundColor="rgb(255, 255, 255, 0.4)"
-                    padding="0.5rem"
-                    borderRadius="0.5rem"
-                    justifyContent="center"
-                    _hover={{
-                      background: "rgb(255, 255, 255, 0.6)",
-                    }}
-                  >
-                    <Flex
-                      width="90%"
-                      justifyContent="space-between"
-                      align="center"
-                    >
-                      <HStack>
-                        <Avatar name={name} src={src} />
-                        <Text>{name}</Text>
-                      </HStack>
+              <AccordionPanel>
+                <VStack>
+                  {Object.entries(members[key[0]]).map(
+                    ([name, src], index) => (
+                      <Flex
+                        key={key[0] + index}
+                        width="100%"
+                        backgroundColor="rgb(255, 255, 255, 0.4)"
+                        padding="0.5rem"
+                        borderRadius="0.5rem"
+                        justifyContent="center"
+                        _hover={{
+                          background: "rgb(255, 255, 255, 0.6)",
+                        }}
+                      >
+                        <Flex
+                          width="90%"
+                          justifyContent="space-between"
+                          align="center"
+                        >
+                          <HStack>
+                            <Avatar name={name} src={src} />
+                            <Text>{name}</Text>
+                          </HStack>
 
-                      <Badge colorScheme="blue">Select</Badge>
-                    </Flex>
-                  </Flex>
-                ))}
-              </VStack>
-            </AccordionPanel>
-          </AccordionItem>
-        ))}
-      </Accordion>
+                          <Badge colorScheme="blue">Select</Badge>
+                        </Flex>
+                      </Flex>
+                    )
+                  )}
+                </VStack>
+              </AccordionPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </Box>
     </Flex>
   );
 }
