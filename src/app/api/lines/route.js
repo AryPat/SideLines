b4e18f223cfe37e12378dd6dc5832730fe5ea180
Arypat <aryan.patel70@live.ca>
2024-01-15 23:45:54 -0500

    check?

--- src/app/api/lines/route.js
+++ src/app/api/lines/route.js
@@ -1,9 +1,9 @@
-import { connect } from "http2";
 import { NextResponse } from "next/server";
 const { sequelize, connectDB } = require("../../../models/db.js");
 const { Line, syncDB } = require("../../../models/model.js");
 
 export async function POST(req) {
-  connectDB();
-  syncDB();
+  return NextResponse.json({
+    count: process.env.PROD_DATABASE_NAME,
+  });
 }
