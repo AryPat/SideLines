9260e6140e301128e50b9723f45f1bbe011f89ff
Arypat <aryan.patel70@live.ca>
2024-01-15 23:54:09 -0500

    again

--- src/app/api/lines/count/route.js
+++ src/app/api/lines/count/route.js
@@ -3,6 +3,11 @@ const { sequelize, connectDB } = require("../../../../models/db.js");
 const { Line, syncDB } = require("../../../../models/model.js");
 
 export async function POST(req) {
-  connectDB();
-  syncDB();
+  req = await req.json();
+
+  const data = req.data ? await Line.count(req.data) : await Line.count();
+
+  return NextResponse.json({
+    count: data,
+  });
 }
--- src/app/api/lines/route.js
+++ src/app/api/lines/route.js
@@ -3,7 +3,12 @@ const { sequelize, connectDB } = require("../../../models/db.js");
 const { Line, syncDB } = require("../../../models/model.js");
 
 export async function POST(req) {
+  req = await req.json();
+
+  const data = req.data ? await Line.findAll(req.data) : await Line.findAll();
+
   return NextResponse.json({
-    count: process.env.PROD_DATABASE_NAME,
+    count: data.length,
+    result: data,
   });
 }
